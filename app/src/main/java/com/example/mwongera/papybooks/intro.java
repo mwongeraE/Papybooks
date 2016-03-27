89662a6a02698a8a63f83e115fac932a06c6a4d0
mwongeraE <evansmwongera@gmail.com>
2016-03-27 16:17:38 +0300

    libs

--- app/src/main/java/com/example/mwongera/papybooks/intro.java
+++ app/src/main/java/com/example/mwongera/papybooks/intro.java
@@ -1,7 +1,55 @@
 package com.example.mwongera.papybooks;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
 /**
  * Created by mwongera on 3/27/16.
  */
-public class intro {
+public class intro extends BaseAppIntro {
+
+
+    @Override
+    public void init(Bundle savedInstanceState) {
+        addSlide(SampleSlide.newInstance(R.layout.intro));
+        addSlide(SampleSlide.newInstance(R.layout.intro2));
+        addSlide(SampleSlide.newInstance(R.layout.intro3));
+        addSlide(SampleSlide.newInstance(R.layout.intro4));
+
+        setZoomAnimation();
+    }
+
+    private void loadMainActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onSkipPressed() {
+        loadMainActivity();
+        Toast.makeText(getApplicationContext(), getString(R.string.skip), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNextPressed() {
+
+    }
+
+    @Override
+    public void onDonePressed() {
+        loadMainActivity();
+    }
+
+    @Override
+    public void onSlideChanged() {
+
+    }
+
+    public void getStarted(View v){
+        loadMainActivity();
+    }
+}
+
 }
