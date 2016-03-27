a4384b65a3ab35290d9fa6fb25e536a64a66588d
mwongeraE <evansmwongera@gmail.com>
2016-03-27 15:26:41 +0300

    splash activity

--- app/src/main/java/com/example/mwongera/papybooks/splash.java
+++ app/src/main/java/com/example/mwongera/papybooks/splash.java
@@ -1,7 +1,38 @@
 package com.example.mwongera.papybooks;
 
-/**
- * Created by mwongera on 3/27/16.
- */
-public class splash {
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+
+public class splash extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread() {
+            public void run() {
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(splash.this,Main.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+
+    }
 }
